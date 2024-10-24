73549032caa891afadba4f1e03cf336fac84a000
Irving Leonard <irvingleonard@gmail.com>
2024-10-24 09:16:17 -0400

    Cleanup posix

--- pathlib_/posix.py
+++ pathlib_/posix.py
@@ -1,30 +1,33 @@
 #!python
-'''A reimplementation of the python standard library's pathlib.
-The original pathlib module seems to revolve around the idea that the path is a string, and then it can't decide if the paths are inmutable or not. This module works with a different paradigm: a path is a sequence of individual components divided by a "separator" and such sequence is inmutable.
+"""A reimplementation of the python standard library's pathlib.
+The original pathlib module seems to revolve around the idea that the path is a string, and then it can't decide if the paths are immutable or not. This module works with a different paradigm: a path is a sequence of individual components divided by a "separator" and such sequence is immutable.
 
 This submodule contains the specifics for POSIX systems.
-'''
+"""
 
-import logging
+from logging import getLogger
 
 from ._base import BasePurePath, __version__
 from ._local import BaseOSPath
 
-LOGGER = logging.getLogger(__name__)
+LOGGER = getLogger(__name__)
 
 
 class PurePosixPath(BasePurePath):
+	"""
+	
+	"""
 
 	@classmethod
 	def _parse_path(cls, path):
-		'''Local parsing logic
+		"""Local parsing logic
 		Should implement whatever logic is needed to parse the provided path string into a tuple (drive, root, tail)
 
 		Drive and/or root could be empty, but both should be strings. Tail should be a sequence (could be empty too).
 		The empty path would yield ('', '', [])
 
 		The method should not try to simplify the path (resolve globbing, remove separator repetitions, etc.). The class must be able to recreate the original values, which becomes impossible if any part of it is removed here.
-		'''
+		"""
 
 		if path:
 			if path[0] == cls.SEPARATOR:
@@ -34,19 +37,21 @@ class PurePosixPath(BasePurePath):
 				root = ''
 				tail = path
 			tail = tail.split(cls.SEPARATOR) if tail else []
-			return ('', root, tail)
+			return '', root, tail
 		else:
-			return ('', '', [])
+			return '', '', []
 
 	def as_posix(self):
-		'''Return the string representation of the path with forward (/) slashes.'''
+		"""
+		Return the string representation of the path with forward (/) slashes.
+		"""
 
 		return str(self)
 
 	def as_uri(self):
-		'''Return the path as a URI.
+		"""Return the path as a URI.
 		The logic is local, to be defined by the path syntax.
-		'''
+		"""
 
 		if not self.is_absolute():
 			raise ValueError("relative path can't be expressed as a file URI")
@@ -54,7 +59,14 @@ class PurePosixPath(BasePurePath):
 
 
 class PosixPath(BaseOSPath, PurePosixPath):
+	"""
+	
+	"""
 	
 	@classmethod
 	def new_instance(cls, *args, **kwargs):
+		"""
+		
+		"""
+		
 		return cls(*args, **kwargs).stat()
