7acb620447bd535d7242f01e5910dc5edd18c84e
Irving Leonard <irvingleonard@gmail.com>
2024-10-24 07:33:46 -0400

    Cleaned init

--- pathlib_/__init__.py
+++ pathlib_/__init__.py
@@ -1,6 +1,6 @@
 #!python
 """A reimplementation of the python standard library's pathlib.
-The original pathlib module seems to revolve around the idea that the path is a string, and then it can't decide if the paths are inmutable or not. This module works with a different paradigm: a path is a sequence of individual components divided by a "separator" and such sequence is inmutable.
+The original pathlib module seems to revolve around the idea that the path is a string, and then it can't decide if the paths are immutable or not. This module works with a different paradigm: a path is a sequence of individual components divided by a "separator" and such sequence is immutable.
 
 This module also tries to avoid assumptions about paths: people can come up with all kind of ideas of how a path would look like in system X, this module tries to avoid the dichotomy of POSIX or Windows. The classes on this file do work under the assumption of such dichotomy and are basically factories that build path objects from the "right" classes.
 """
